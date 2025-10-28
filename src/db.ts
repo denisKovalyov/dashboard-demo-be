a3d8cefd555b1d9a44312f3a38b0c7cbe01d01b3
Denys Kovalyov <kovalovdenis@gmail.com>
2025-10-28 18:41:06 +0200

    Update db path resolving

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,12 @@
 import Database from 'better-sqlite3';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Database(process.env.DB_FILE_PATH);
+const dbPath = process.env.DB_FILE_PATH
+  ? path.join(process.cwd(), process.env.DB_FILE_PATH)
+  : path.join(process.cwd(), 'db', 'database.sqlite');
+
+const db = new Database(dbPath);
 
 export default db;
